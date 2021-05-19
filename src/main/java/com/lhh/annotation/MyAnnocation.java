463260a00ebfdf7076e448d5d783afbb28fd4f78
liuhuanhuan963019 <2432783449@qq.com>
2021-05-20 00:18:09 +0800

    重复注解

---
+++ src/main/java/com/lhh/annotation/MyAnnocation.java
@@ -0,0 +1,17 @@
+package com.lhh.annotation;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+
+@Repeatable(MyAnnotions.class)
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface MyAnnocation {
+
+    String value() default "lhh";
+}
